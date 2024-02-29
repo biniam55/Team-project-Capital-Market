cd7633063b62e70df367b6be880a88384c1983be
Matthew-uk <esaduviedede@gmail.com>
2024-02-29 02:14:17 +0100

    Almost done with Dashboard

---
+++ components/dashboardLayout.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState } from "react";
+import Logo from "./logo";
+import Nav from "./nav";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <div className="min-h-screen font-poppins">
+      <div className="block md:hidden flex items-center p-4 border-r-2 rounded">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="flex-grow p-4 border-l-4 border-gray-400">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
